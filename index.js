c45585adc6c2876d00c66d3742bf4a043480a9e4
Scotxh <105351945+Scotxh@users.noreply.github.com>
2022-07-18 18:41:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -145,16 +145,16 @@ Kon = await getBuffer(`https://hardianto.xyz/api/welcome3?profile=${encodeURICom
 Tol = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURIComponent(ppuser)}&name=${encodeURIComponent(nama)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`)
                 if (anu.action == 'add') {
                     GojoMdNx.sendMessage(anu.id, { image: Kon, contextInfo: { mentionedJid: [num] }, caption: `
-⭐✑ Hi👋 @${num.split("@")[0]},
-⭐✑ Welcome To ${metadata.subject}
+✧ HII I'M SCOTCH ✧ @${num.split("@")[0]},
+✧ Welcome To ${metadata.subject}
 
-⭐✑ Description: ${metadata.desc}
+✧ Description: ${metadata.desc}
 
-⭐✑ Welcome To Our Comfortable Happy😋, Sometimes Loud😜, Usually Messy🤥, Full Of Love🥰, HOME😌!!`} )
+✧ HELLO DEAR WELCOME THANK YOU FOR JOINING PLEASE FOLLOW RULES HAVE A GOOD DAY 💜💫💌, Full Of Love, HOME`} )
                 } else if (anu.action == 'remove') {
-                    GojoMdNx.sendMessage(anu.id, { image: Tol, contextInfo: { mentionedJid: [num] }, caption: `⭐✑ @${num.split("@")[0]} Left ${metadata.subject}
+                    GojoMdNx.sendMessage(anu.id, { image: Tol, contextInfo: { mentionedJid: [num] }, caption: `✧ @${num.split("@")[0]} Left ${metadata.subject}
 
-⭐✑ I'm Not Sure If It Was A Goodbye Charm, But It Was Fun While It Lasted 😌✨` })
+✧ Sayonara Join Again If You Have To Come. But It Was Fun While It Lasted 😌✨` })
                 }
             }
         } catch (err) {
